b3907b6d4328dd6aaa72e11c6da636b43334988a
Shane Flynn <sflynn248@gmail.com>
2021-04-22 20:11:21 -0500

    finished

--- Organism.h
+++ Organism.h
@@ -8,25 +8,47 @@ class Organism 				{
 private:
 	std::unique_ptr<char[]> genotype;
 	int fitness;
+	int genoTypeSize;
 public:
 	Organism();
 	Organism(std::string, const int&);
+	Organism(const Organism&);
+	void operator= (const Organism&);
 	char getAlleleAt(const int&) const;
+	void mutateAlleleAt(const int&, const char);
 	void setFitness(const int&);
 	int getFitness() const;
 };
 Organism::Organism(){
 	genotype = nullptr;
+	genoTypeSize = 0;
 	fitness = 0;
 }
 Organism::Organism(std::string genotype, const int& geno_size) 	{
 	this->genotype = std::make_unique<char[]>(geno_size);
 	fitness = 0;
+	genoTypeSize = geno_size;
 	for (int i = 0; i < geno_size; i++)
 		this->genotype[i] = genotype[i];
 
 }
-
+Organism::Organism(const Organism& organ) {
+	fitness = organ.fitness;
+	genoTypeSize = organ.genoTypeSize;
+	genotype.reset();
+	genotype = std::make_unique<char[]>(genoTypeSize);
+	for (int i = 0; i < genoTypeSize; i++)
+		genotype[i] = organ.genotype[i];
+}
+void Organism::operator= (const Organism& organ) {
+	fitness = organ.fitness;
+	genoTypeSize = organ.genoTypeSize;
+	genotype.reset();
+	genotype = std::make_unique<char []>(genoTypeSize);
+	for (int i = 0; i < genoTypeSize; i++)
+		genotype[i] = organ.genotype[i];
+	
+}
 char Organism::getAlleleAt(const int& index) const {
 	return genotype[index];
 }
@@ -34,7 +56,9 @@ char Organism::getAlleleAt(const int& index) const {
 void Organism::setFitness(const int& fitness) {
 	this->fitness = fitness;
 }
-
+void Organism::mutateAlleleAt(const int& index, const char mutation) {
+	genotype[index] = mutation;
+}
 int Organism::getFitness() const { return fitness; }
 #endif // !ORGANISM_H
 
--- main.cpp
+++ main.cpp
@@ -3,23 +3,23 @@
 #include <random>
 #include <string>
 #include "Organism.h"
-const int GENERATION_SIZE = 100;
+const int GENERATION_SIZE = 1000;
 const int MAX_GENERATIONS = 1000;
-const int PARENT_COUNT = 2;
+const int PARENT_COUNT = 20;
 const int FITNESS_THRESHOLD = 10; // can do a ratio of correct letters to incorrect ones
 const int GENOTYPE_SIZE = 10;
-const double MUTATION_RATE = 0.05;
+const double MUTATION_RATE = 0.1;
 char getRandomChar();
 std::string getRandomGenotype();
 void geneticAlgorithm();
 void evaluate(Organism&);
 Organism breed(std::unique_ptr<Organism[]>&);
 void mutate(Organism&);
+void insertionSort(std::unique_ptr<Organism[]>&);
+template <typename T>
+T getRandomNumber(int, int);
 
 int main() {
-	for (int i = 0; i < 10; i++) 		{
-		std::cout << getRandomChar() << std::endl;
-	}
 	geneticAlgorithm();
 	std::cin.get();
 	return 0;
@@ -37,6 +37,12 @@ void geneticAlgorithm() {
 			evaluate(generation[i]);
 		}
 
+
+		insertionSort(generation);
+		for (int i = 0; i < PARENT_COUNT; i++) 		{
+			std::cout << generation[i].getFitness() << " ";
+		}
+		std::cout << std::endl;
 		//Sort decending
 		//sortDecending(generation) by fitness
 
@@ -48,7 +54,7 @@ void geneticAlgorithm() {
 
 		std::unique_ptr<Organism[]> parents = std::make_unique<Organism[]>(PARENT_COUNT);
 		for (int p = 0; p < PARENT_COUNT; p++) 		{
-			parents[p] = std::move(generation[p]);
+			parents[p] = generation[p];
 		}
 		
 		std::unique_ptr<Organism[]> nextGeneration = std::make_unique<Organism[]>(GENERATION_SIZE);
@@ -59,8 +65,8 @@ void geneticAlgorithm() {
 		for (int i = 0; i < GENERATION_SIZE; i++) 		{
 			mutate(nextGeneration[i]);
 		}
-		generation.release
-		generation = nextGeneration;
+		generation.reset();
+		generation = std::move(nextGeneration);
 	}
 }
 
@@ -78,19 +84,27 @@ void evaluate(Organism& organ) {
 }
 
 Organism breed(std::unique_ptr<Organism[]>& parents) {
-	Organism organ;
-	return organ;
+	std::unique_ptr<char[]> genotype = std::make_unique<char[]>(GENOTYPE_SIZE);
+
+	for (int i = 0; i < GENOTYPE_SIZE; i++) 		{
+		Organism parentToInheritFrom = parents[getRandomNumber<int>(0, PARENT_COUNT-1)];
+		genotype[i] = parentToInheritFrom.getAlleleAt(i);
+	}
+	
+	return Organism((std::string)genotype.get(), GENOTYPE_SIZE);
 }
 
 void mutate(Organism& organ) {
 	for (int i = 0; i < GENOTYPE_SIZE; i++) 		{
-
+		if ((getRandomNumber<int>(0,100) / 100.f)  < MUTATION_RATE) 	{
+			organ.mutateAlleleAt(i, getRandomChar());
+		}
 	}
 }
 
 std::string getRandomGenotype() {
-	std::string genotype = "";
-	for (int i = 0; i < GENOTYPE_SIZE; i++)
+	thread_local std::string genotype = "";
+	for (thread_local int i = 0; i < GENOTYPE_SIZE; i++)
 		genotype += getRandomChar();
 	
 	return genotype;
@@ -100,4 +114,26 @@ char getRandomChar() { //get random number between 0 < ri <= r
 	thread_local std::mt19937 rng(rand()); //seed
 	std::uniform_int_distribution<int> generator(65, 90); // min, max
 	return static_cast<char>(generator(rng));
+}
+
+template<typename T>
+T getRandomNumber(int min, int max) {
+	srand(time(0));
+	thread_local std::mt19937 rng(rand()); //seed
+	std::uniform_int_distribution<T> generator(min, max); // min, max
+	return generator(rng);
+}
+
+void insertionSort(std::unique_ptr<Organism []>& generation) {
+
+	for (int i = 1; i < GENERATION_SIZE; i++) 		{
+		int j = i;
+		Organism temp = generation[j];
+
+		while (j > 0 && temp.getFitness() > generation[j-1].getFitness()) 	{
+			generation[j] = generation[j - 1];
+			j--;
+			generation[j] = temp;
+		}
+	}
 }
